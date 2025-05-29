5b4c2252f31d3b2c92631ba4f3e9cb93fea8b8c5
nabin--raj <gith60707@gmail.com>
2025-05-29 22:11:22 +0545

    pussing app

---
+++ Password_manager/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="h-[6vh] bg-slate-900 text-white flex items-center text-xl font-bold justify-around">
+      <div className="right ">
+        <span className="text-green-700">&lt;</span>
+        <span>Pass</span>
+        <span className="text-green-700">Po/&gt;</span>
+      </div>
+
+      <div className="left">
+        <button
+        className="bg-green-700 flex justify-around items-center rounded-full px-2 outline ring ring-1"
+        >
+          <lord-icon
+            src="https://cdn.lordicon.com/jjxzcivr.json"
+            trigger="hover"
+            stroke="bold"
+            colors="primary:#ffffff,secondary:#ffffff"
+          ></lord-icon>
+          <span className="text-sm font-roboto "> GitHub</span>
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
